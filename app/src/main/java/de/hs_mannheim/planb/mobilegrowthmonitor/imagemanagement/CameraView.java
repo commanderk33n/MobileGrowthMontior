09236762fe1dc57a1fae5a0abe52cc62a8094002
t.mueller1 <billiejoe1985>
2016-05-05 10:02:33 +0200

    removed CameraIntent and added complete android.hardware.camera implementation

--- app/src/main/java/de/hs_mannheim/planb/mobilegrowthmonitor/imagemanagement/CameraView.java
+++ app/src/main/java/de/hs_mannheim/planb/mobilegrowthmonitor/imagemanagement/CameraView.java
@@ -1,51 +1,443 @@
 package de.hs_mannheim.planb.mobilegrowthmonitor.imagemanagement;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import de.hs_mannheim.planb.mobilegrowthmonitor.R;
 import de.hs_mannheim.planb.mobilegrowthmonitor.pinlock.BaseActivity;
 
 public class CameraView extends BaseActivity {
+    private static String TAG = "CameraView";
 
-    private ImageView capturedImage;
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private View mCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_view);
-
-        Button btnTakePicture = (Button) findViewById(R.id.btn_takePicture);
-
-        capturedImage = (ImageView) findViewById(R.id.iv_capturedImage);
+        mCameraView = findViewById(R.id.camera_preview);
 
 
-        if (btnTakePicture != null) {
-            btnTakePicture.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openCamera();
-                }
-            });
+        boolean opened = safeCameraOpenInView();
+
+        if (!opened) {
+            Log.d(TAG, "Error, Camera failed to open");
         }
-    }
 
-    private void openCamera() {
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 0);
+        Button captureButton = (Button) findViewById(R.id.btn_capture);
+        if (captureButton != null) {
+            captureButton.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // get an image from the camera
+                            mCamera.takePicture(null, null, mPicture);
+                        }
+                    }
+            );
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            Bitmap bp = (Bitmap) data.getExtras().get("data");
-            capturedImage.setImageBitmap(bp);
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releaseCameraAndPreview();
+    }
+
+
+    /**
+     * Recommended "safe" way to open the camera.
+     *
+     * @return
+     */
+    private boolean safeCameraOpenInView() {
+        boolean qOpened = false;
+        releaseCameraAndPreview();
+        mCamera = getCameraInstance();
+        qOpened = (mCamera != null);
+        if (qOpened == true) {
+            mPreview = new CameraPreview(this.getBaseContext(), mCamera, mCameraView);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+            mPreview.startCameraPreview();
+        }
+        return qOpened;
+    }
+
+    /**
+     * Safe method for getting a camera instance.
+     *
+     * @return
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /**
+     * Clear any existing preview / camera.
+     */
+    private void releaseCameraAndPreview() {
+
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+        if (mPreview != null) {
+            mPreview.destroyDrawingCache();
+            mPreview.mCamera = null;
         }
     }
 
+    /**
+     * Surface on which the camera projects it's capture results.
+     */
+    class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+        // SurfaceHolder
+        private SurfaceHolder mHolder;
+
+        // Our Camera.
+        private Camera mCamera;
+
+        // Parent Context.
+        private Context mContext;
+
+        // Camera Sizing (For rotation, orientation changes)
+        private Camera.Size mPreviewSize;
+
+        // List of supported preview sizes
+        private List<Camera.Size> mSupportedPreviewSizes;
+
+        // Flash modes supported by this camera
+        private List<String> mSupportedFlashModes;
+
+        // View holding this camera.
+        private View mCameraView;
+
+
+        public CameraPreview(Context context, Camera camera, View cameraView) {
+            super(context);
+
+            // Capture the context
+            mCameraView = cameraView;
+            mContext = context;
+            setCamera(camera);
+
+            // Install a SurfaceHolder.Callback so we get notified when the
+            // underlying surface is created and destroyed.
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+            mHolder.setKeepScreenOn(true);
+            // deprecated setting, but required on Android versions prior to 3.0
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        /**
+         * Begin the preview of the camera input.
+         */
+        public void startCameraPreview() {
+            try {
+                CameraInfo mCameraInfo = new CameraInfo();
+                Camera.getCameraInfo(0, mCameraInfo);
+                mCamera.setDisplayOrientation(getCorrectCameraOrientation(mCameraInfo, mCamera));
+                mCamera.getParameters().setRotation(getCorrectCameraOrientation(mCameraInfo, mCamera));
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Extract supported preview and flash modes from the camera.
+         *
+         * @param camera
+         */
+        private void setCamera(Camera camera) {
+            mCamera = camera;
+            mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+            mSupportedFlashModes = mCamera.getParameters().getSupportedFlashModes();
+            // Set the camera to Auto Flash mode.
+            if (mSupportedFlashModes != null && mSupportedFlashModes.contains(Camera.Parameters.FLASH_MODE_AUTO)) {
+                Camera.Parameters parameters = mCamera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+                mCamera.setParameters(parameters);
+            }
+
+            requestLayout();
+        }
+
+        /**
+         * The Surface has been created, now tell the camera where to draw the preview.
+         *
+         * @param holder
+         */
+        public void surfaceCreated(SurfaceHolder holder) {
+
+            try {
+                mCamera.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Dispose of the camera preview.
+         *
+         * @param holder
+         */
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            if (mCamera != null) {
+                mCamera.stopPreview();
+            }
+        }
+
+        /**
+         * React to surface changed events
+         *
+         * @param holder
+         * @param format
+         * @param w
+         * @param h
+         */
+        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+
+            if (mHolder.getSurface() == null) {
+                return;
+            }
+            try {
+                Camera.Parameters parameters = mCamera.getParameters();
+                mCamera.setParameters(parameters);
+                startCameraPreview();
+                parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+                if (mPreviewSize != null) {
+                    Camera.Size previewSize = mPreviewSize;
+                    parameters.setPreviewSize(previewSize.width, previewSize.height);
+                }
+
+                mCamera.setParameters(parameters);
+                mCamera.startPreview();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Calculate the measurements of the layout
+         *
+         * @param widthMeasureSpec
+         * @param heightMeasureSpec
+         */
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+            final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+            setMeasuredDimension(width, height);
+
+            if (mSupportedPreviewSizes != null) {
+                mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+            }
+        }
+
+        /**
+         * Update the layout based on rotation and orientation changes.
+         *
+         * @param changed
+         * @param left
+         * @param top
+         * @param right
+         * @param bottom
+         */
+        @Override
+        protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+            if (changed) {
+                final int width = right - left;
+                final int height = bottom - top;
+
+                int previewWidth = width;
+                int previewHeight = height;
+
+                if (mPreviewSize != null) {
+                    Display display = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+                    switch (display.getRotation()) {
+                        case Surface.ROTATION_0:
+                            previewWidth = mPreviewSize.height;
+                            previewHeight = mPreviewSize.width;
+                            mCamera.setDisplayOrientation(90);
+                            break;
+                        case Surface.ROTATION_90:
+                            previewWidth = mPreviewSize.width;
+                            previewHeight = mPreviewSize.height;
+                            break;
+                        case Surface.ROTATION_180:
+                            previewWidth = mPreviewSize.height;
+                            previewHeight = mPreviewSize.width;
+                            break;
+                        case Surface.ROTATION_270:
+                            previewWidth = mPreviewSize.width;
+                            previewHeight = mPreviewSize.height;
+                            mCamera.setDisplayOrientation(180);
+                            break;
+                    }
+                }
+
+                final int scaledChildHeight = previewHeight * width / previewWidth;
+                mCameraView.layout(0, height - scaledChildHeight, width, height);
+            }
+        }
+
+        /**
+         * @param sizes
+         * @param width
+         * @param height
+         * @return
+         */
+        private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int width, int height) {
+            // Source: http://stackoverflow.com/questions/7942378/android-camera-will-not-work-startpreview-fails
+            Camera.Size optimalSize = null;
+
+            final double ASPECT_TOLERANCE = 0.1;
+            double targetRatio = (double) height / width;
+
+            // Try to find a size match which suits the whole screen minus the menu on the left.
+            for (Camera.Size size : sizes) {
+
+                if (size.height != width) continue;
+                double ratio = (double) size.width / size.height;
+                if (ratio <= targetRatio + ASPECT_TOLERANCE && ratio >= targetRatio - ASPECT_TOLERANCE) {
+                    optimalSize = size;
+                }
+            }
+            return optimalSize;
+        }
+
+        public int getCorrectCameraOrientation(CameraInfo info, Camera camera) {
+
+            int rotation = CameraView.this.getWindowManager().getDefaultDisplay().getRotation();
+            int degrees = 0;
+
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    degrees = 0;
+                    break;
+
+                case Surface.ROTATION_90:
+                    degrees = 90;
+                    break;
+
+                case Surface.ROTATION_180:
+                    degrees = 180;
+                    break;
+
+                case Surface.ROTATION_270:
+                    degrees = 270;
+                    break;
+
+            }
+
+            int result;
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                result = (info.orientation + degrees) % 360;
+                result = (360 - result) % 360;
+            } else {
+                result = (info.orientation - degrees + 360) % 360;
+            }
+
+            return result;
+        }
+    }
+
+
+    /**
+     * Picture Callback for handling a picture capture and saving it out to a file.
+     */
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                Toast.makeText(CameraView.this, "Image retrieval failed.", Toast.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+
+                // Restart the camera preview.
+                safeCameraOpenInView();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    /**
+     * Used to return the camera File output.
+     *
+     * @return
+     */
+    private File getOutputMediaFile() {
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MobileGrowthMonitor");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "Required media storage does not exist");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_" + timeStamp + ".jpg");
+        Toast.makeText(CameraView.this, "Success! Your picture has been saved!", Toast.LENGTH_SHORT)
+                .show();
+
+        return mediaFile;
+    }
 }
--- app/src/main/java/de/hs_mannheim/planb/mobilegrowthmonitor/imageprocessing/PictureFileHandler.java
+++
@@ -1,7 +0,0 @@
-package de.hs_mannheim.planb.mobilegrowthmonitor.imageprocessing;
-
-/**
- * Created by eikood on 05.05.2016.
- */
-public class PictureFileHandler {
-}
