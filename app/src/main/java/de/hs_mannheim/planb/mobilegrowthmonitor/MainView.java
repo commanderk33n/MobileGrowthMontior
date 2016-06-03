78fa34607ef7dd24ed8c1e4d06fb1b48a3d897b6
Laura <Most-Laura@gmx.de>
2016-06-03 16:08:37 +0200

    On Android 6, permission are now granted at runtime.

--- app/src/main/java/de/hs_mannheim/planb/mobilegrowthmonitor/MainView.java
+++ app/src/main/java/de/hs_mannheim/planb/mobilegrowthmonitor/MainView.java
@@ -5,19 +5,25 @@ package de.hs_mannheim.planb.mobilegrowthmonitor;
  * Shows all Profiles - add new profiles
  */
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import java.io.File;
@@ -30,7 +36,7 @@ import de.hs_mannheim.planb.mobilegrowthmonitor.pinlock.AppLockView;
 import de.hs_mannheim.planb.mobilegrowthmonitor.pinlock.BaseActivity;
 import de.hs_mannheim.planb.mobilegrowthmonitor.pinlock.LockManager;
 
-public class MainView extends BaseActivity implements Listener {
+public class MainView extends BaseActivity implements Listener, PermissionDialogFragment.NoticeDialogListener {
     public static final String TAG = MainView.class.getSimpleName();
 
     private RecyclerView recyclerView;
@@ -41,6 +47,10 @@ public class MainView extends BaseActivity implements Listener {
     private MenuItem onOffPinLock;
     private MenuItem changePin;
 
+    private static final int PERMISSIONS_REQUEST_CAMERA = 1;
+    private static final int PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE =2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,36 @@ public class MainView extends BaseActivity implements Listener {
         setContentView(R.layout.main_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                    DialogFragment newFragment =  new PermissionDialogFragment();
+                    Bundle args = new Bundle();
+                    args.putInt("permission", PERMISSIONS_REQUEST_CAMERA);
+                    newFragment.setArguments(args);
+                    newFragment.show(getFragmentManager(), "permissionDialog");
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                            PERMISSIONS_REQUEST_CAMERA);
+                }
+                return;
+            }
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    DialogFragment newFragment =  new PermissionDialogFragment();
+                    Bundle args = new Bundle();
+                    args.putInt("permission", PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    newFragment.setArguments(args);
+                    newFragment.show(getFragmentManager(), "permissionDialog");
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return;
+            }
+        }
 
 
         dbHelper = DbHelper.getInstance(getApplicationContext());
@@ -179,4 +219,33 @@ public class MainView extends BaseActivity implements Listener {
         dbHelper.close();
     }
 
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        final int permission = dialog.getArguments().getInt("permission");
+        switch(permission){
+            case PERMISSIONS_REQUEST_CAMERA:
+                ActivityCompat.requestPermissions(getParent(),
+                        new String[]{Manifest.permission.CAMERA},
+                        PERMISSIONS_REQUEST_CAMERA);
+                break;
+            case PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                ActivityCompat.requestPermissions(getParent(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                break;
+            default:
+                break;
+        }
+
+
+
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        Activity activity = getParent();
+        if (activity != null) {
+            activity.finish();
+        }
+    }
 }
