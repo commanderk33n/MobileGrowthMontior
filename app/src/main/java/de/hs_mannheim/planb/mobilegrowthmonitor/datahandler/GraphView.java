f0bb03e1968d570fd69abf5a6336136124eae951
eikood <hanfaplast@googlemail.com>
2016-06-05 15:56:25 +0200

    bmi - diagram now has x-axis labeld with date of measuring

--- app/src/main/java/de/hs_mannheim/planb/mobilegrowthmonitor/datahandler/GraphView.java
+++ app/src/main/java/de/hs_mannheim/planb/mobilegrowthmonitor/datahandler/GraphView.java
@@ -1,22 +1,26 @@
 package de.hs_mannheim.planb.mobilegrowthmonitor.datahandler;
 
-import android.graphics.DashPathEffect;
-import android.os.Bundle;
 
-import com.androidplot.util.PixelUtils;
+import android.os.Bundle;
+import android.util.Log;
+
 import com.androidplot.xy.CatmullRomInterpolator;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.XYStepMode;
 
-import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.text.Format;
 import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -27,9 +31,14 @@ import de.hs_mannheim.planb.mobilegrowthmonitor.pinlock.BaseActivity;
 
 public class GraphView extends BaseActivity {
 
-    private XYPlot plot;
+    private static final String TAG = GraphView.class.getSimpleName();
+
+    private XYPlot mPlot;
     private int profileId;
     private DbHelper dbHelper;
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,33 +50,26 @@ public class GraphView extends BaseActivity {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         ArrayList<MeasurementData> measurements = dbHelper.getAllMeasurements(profileId);
 
-        List<Double> bmi = new ArrayList<>();
+        List<Date> dateList = new ArrayList<>();
+        List<Double> bmiList = new ArrayList<>();
         for (MeasurementData md : measurements) {
-            bmi.add(md.height * md.weight);
-
-            /*
-            Calendar date = new GregorianCalendar();
+            bmiList.add(md.height * md.weight);
+            System.out.println("Height: " + md.height + "Weight: " + md.weight);
+            Calendar calendar = new GregorianCalendar();
             try {
-                date.setTime(format.parse(md.date));
+                calendar.setTime(format.parse(md.date));
             } catch (ParseException e) {
                 e.printStackTrace();
             }
-            long l = date.getTimeInMillis();
-            bmi.add(Double.longBitsToDouble(l));
-            System.out.println("TimeStamp: " + l);
-            */
-
+            dateList.add(calendar.getTime());
+            System.out.println("Timestamp: " +calendar.getTimeInMillis());
         }
 
+        final Date[] dateArray = dateList.toArray(new Date[dateList.size()]);
         // initialize our XYPlot reference:
-        plot = (XYPlot) findViewById(R.id.plot);
+        mPlot = (XYPlot) findViewById(R.id.plot);
 
-
-        // turn the above arrays into XYSeries':
-
-        XYSeries bmiSeries = new SimpleXYSeries(bmi,
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "BMI");
-
+        XYSeries bmiSeries = new SimpleXYSeries(bmiList, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "BMI");
 
         // create formatters to use for drawing a series using LineAndPointRenderer
         // and configure them from xml:
@@ -77,7 +79,6 @@ public class GraphView extends BaseActivity {
                 R.xml.line_point_formatter_with_labels);
 
 
-
         // just for fun, add some smoothing to the lines:
         // see: http://androidplot.com/smooth-curves-and-androidplot/
         bmiFormat.setInterpolationParams(
@@ -85,14 +86,51 @@ public class GraphView extends BaseActivity {
 
 
         // add a new series' to the xyplot:
-        plot.addSeries(bmiSeries, bmiFormat);
+        mPlot.addSeries(bmiSeries, bmiFormat);
 
- 
+        // draw a domain tick for each year:
+        mPlot.setDomainStep(XYStepMode.SUBDIVIDE, dateArray.length);
+        mPlot.setRangeValueFormat(new DecimalFormat("0"));
+
+        mPlot.setDomainValueFormat(new Format() {
+
+            // create a simple date format that draws on the year portion of our timestamp.
+            // see http://download.oracle.com/javase/1.4.2/docs/api/java/text/SimpleDateFormat.html
+            // for a full description of SimpleDateFormat.
+            private SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd");
+
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                int i = ((Number) obj).intValue();
+                Date date = dateArray[i];
+                Log.d(TAG, "formatting date=" + new SimpleDateFormat("MMMM dd, yyyy").format(date));
+                return dateFormat.format(date, toAppendTo, pos);
+            }
+
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+
+            }
+        });
+
+
+
+
         // reduce the number of range labels
-        plot.setTicksPerRangeLabel(3);
+        mPlot.setTicksPerRangeLabel(2);
+        mPlot.setTicksPerDomainLabel(2);
 
         // rotate domain labels 45 degrees to make them more compact horizontally:
-        plot.getGraphWidget().setDomainLabelOrientation(-45);
+        mPlot.getGraphWidget().setDomainLabelOrientation(-45);
+        mPlot.getGraphWidget().setRangeTickLabelWidth(50);
+
+
 
     }
+
+
+
+
 }
+
